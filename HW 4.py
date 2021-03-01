a56b59e7dbca5e178aadadec4592c209d70f5f6e
Haris <khapb29@gmail.com>
2021-03-01 19:56:41 +0200

    Changing HW4

--- HW 4.py
+++ HW 4.py
@@ -34,9 +34,12 @@ car.car_length()
 
 # 3. Determine which class a given Bus object belongs to (Check type of an object)
 
+print(type(car))
+print(issubclass(Bus, Vehicle))
 
 # 4. Determine if School_bus is also an instance of the Vehicle class
 
+print(isinstance(car, Vehicle))
 
 # 5. Create a new class School with get_school_id and number_of_students instance attributes
 
@@ -76,32 +79,100 @@ school_bus.school_bus_color()
 # one of Bear and one of Wolf, make a tuple of it and by using for call their action using the same method.
 
 class Bear:
-    def __init__(self, make_sound):
-        self.make_sound = make_sound
-
-    def sound(self):
-        print(f'Bear say - {self.make_sound}')
-
-
-bear = Bear('I want raspberries')
-bear.sound()
+    def make_sound(self):
+        print('I want raspberries')
 
 
 class Wolf:
-    def __init__(self, make_sound):
-        self.make_sound = make_sound
-
-    def sound(self):
-        print(f'Wolf say - {self.make_sound}')
+    def make_sound(self):
+        print('I want meat')
 
 
-wolf = Wolf('I want meat')
-wolf.sound()
+bear = Bear()
+wolf = Wolf()
+
+Animals = (bear, wolf)
+
+for Animals in Animals:
+    Animals.make_sound()
 
 
-class Animals(Bear, Wolf):
-    pass
+# Magic methods:
+# 8. Create class City with name, population instance attributes, return a new instance only when population > 1500,
+# otherwise return message: "Your city is too small".
+
+class City:
+    def __init__(self, name, population):
+        self.name = name
+        self.population = population
+
+    def __new__(cls, name, population):
+        instance = super(City, cls).__new__(cls)
+        if population > 1500:
+            return instance
+        else:
+            print('Your city is too small')
+
+# 9. Override a printable string representation of the City class and return:
+# The population of the city {name} is {population}
+
+    def __str__(self):
+        print(f'The population of the city {self.name} is {self.population}')
+
+people = City('Kiev', 2884000)
+people.__str__()
+
+# 10*. Override magic method __add__() to perform the additional action as 'multiply' (*)
+# the value which is greater than 10. And perform this add (+) of two instances.
+
+class New:
+    def __init__(self, multiply):
+        self.multiply = multiply
+
+    def __add__(self, new_1):
+        if self.multiply > 10 or new_1.multiply > 10:
+            multiply_1 = self.multiply * new_1.multiply
+        else:
+            multiply_1 = self.multiply + new_1.multiply
+        return New(multiply_1)
+
+    def __str__(self):
+        return f'multiply: {self.multiply}'
 
 
-animals = Animals()
-animals.sound()
+c1 = New(20)
+c2 = New(2)
+c3 = c1 + c2
+print(c3)
+
+# 11. The __call__ method enables Python programmers to write classes where the instances behave like functions and
+# can be called like a function. Create a new class with __call__ method and define this call to return sum.
+
+class Call:
+    def __call__(self, *args):
+        return sum(args)
+
+
+result = Call()
+print(result(-5, 7, 9, 11, 15))
+
+#12*. Making Your Objects Truthy or Falsey Using __bool__().
+#Create class MyOrder with cart and customer instance attributes.
+#Override the __bool__magic method considered to be truthy if the length of the cart list is non-zero.
+
+class MyOrder:
+    def __init__(self, cart, customer):
+        self.cart = cart
+        self.customer = customer
+
+    def __bool__(self):
+        if len(self.cart) > 0:
+            return True
+        else:
+            return False
+
+
+order_1 = MyOrder(['a', 'b', 'c'], 'd')
+order_2 = MyOrder([], 'a')
+print(bool(order_1))
+print(bool(order_2))
